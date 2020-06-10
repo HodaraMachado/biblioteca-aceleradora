08f30e97aaaec84df23a14690e2a2350bc4662b9
Dine <dine.nfg@gmail.com>
2020-06-10 10:01:13 -0300

    Corrigindo apresentação do nome dos autores quando tem mais de um

--- src/main/java/com/aceleradora/biblioteca/Main.java
+++ src/main/java/com/aceleradora/biblioteca/Main.java
@@ -8,28 +8,26 @@ public class Main {
     public static void main(String[] args) {
         apresentaLinha();
 
-        System.out.println("Bem vinda(o) à biblioteca, onde você encontra os melhores livros de Porto Alegre");
+        System.out.println("Bem vinda(o) à biblioteca, onde você encontra os melhores livros de Porto Alegre :)");
 
         //Cria lista com os dois livros que serão apresentados no console para o usuário selecionar.
         // A criação da lista de autores e de livros está dentro do método criaListaComDoisLivros()
         List<Livro> livros = criaListaComDoisLivros();
 
-        //Apresenta o titulo e os autores dos 2 livros
-//        System.out.println("Título do livro 1:" + livros.get(0).getTitulo());
-//        System.out.println("Autoria:" + livros.get(0).getAutor().get(0).getNome());
-//
-//
-//        System.out.println("Título do livro 2:" + livros.get(1).getTitulo());
-//        System.out.println("Autoria:" + livros.get(1).getAutor().get(0).getNome()+","+livros.get(1).getAutor().get(1).getNome());
-
 
         for (int i = 0; i < livros.size(); i++) {
-            System.out.println("Livro "+(i+1));
-            System.out.println("    Título do livro: "+livros.get(i).getTitulo());
+            System.out.println("Livro " + (i + 1));
+            System.out.println("    Título do livro: " + livros.get(i).getTitulo());
+
+            System.out.print("    Autoria: ");
 
             for (int j = 0; j < livros.get(i).getAutor().size(); j++) {
-                System.out.println("    Autoria: "+ livros.get(i).getAutor().get(j).getNome());
+                if (j >= 1) {
+                    System.out.print(", ");
+                }
+                System.out.print(livros.get(i).getAutor().get(j).getNome());
             }
+            System.out.println();
         }
 
         apresentaLinha();
@@ -46,7 +44,7 @@ public class Main {
         List<Livro> livros = new ArrayList<>();
 
         // criando um livro com a lista 'autores' (que só tem a barbara dentro)
-        Livro livro1 = new Livro("Program development in Java",autores);
+        Livro livro1 = new Livro("Program development in Java", autores);
 
         // criando a segunda lista de autores vazia, com nome de 'doisAutores'
         ArrayList<Autor> doisAutores = new ArrayList();
@@ -55,17 +53,15 @@ public class Main {
         doisAutores.add(new Autor("Elisabeth Freeman"));
         doisAutores.add(new Autor("Kathy Sierra"));
 
-        //Criando segundo livro com a lista de autores 'doisAutores'
-        Livro livro2 = new Livro("Use a Cabeça: Padrões de projeto",doisAutores);
 
-        // Adicionando os 2 livros criados dentro da lista de livros
+        Livro livro2 = new Livro("Use a Cabeça: Padrões de projeto", doisAutores);
+
         livros.add(livro1);
         livros.add(livro2);
 
         // retorna a lista de livros com os 2 livros
         return livros;
     }
-
 
 
     private static void apresentaLinha() {
